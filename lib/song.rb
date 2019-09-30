e4363785e183a26529762365d7761c48c9316be5
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-30 18:45:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,4 +4,10 @@ class Song
   @@all = []
   
   def initialize(name, artist=nil, genre=nil)
+    @name = name 
     
+    self.artist = artist if artist
+    
+    self.genre = genre if genre
+    
+  end
